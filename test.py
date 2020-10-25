33d5a78ad8f65cabedfe1e87a8277f69a7290223
phlaxyr <phlaxyzerobane@gmail.com>
2020-10-24 21:49:28 -0500

    update gitignore

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+import pickle
+
+with open('pyetymology/response.pkl', 'rb') as _input:
+    res = pickle.load(_input)
+
+exit(0)
+
 from grandalf.graphs import Vertex,Edge,Graph,graph_core
 V = [Vertex(data) for data in range(10)]
 X = [(0,1),(0,2),(1,3),(2,3),(4,0),(1,4),(4,5),(5,6),(3,6),(3,7),(6,8),(7,8),(8,9),(5,9)]
