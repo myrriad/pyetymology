f83cd5f52ec57f0ce56183f1ef99fcc4013cf3f4
phlaxyr <phlaxyzerobane@gmail.com>
2020-12-14 23:16:34 -0600

    init module

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from pyetymology.main import mainloop
+
+print("main")
+mainloop(cog_search_langs=["English", "Middle English", "Old English", "Proto-Germanic", "Proto-West Germanic",
+                           "Middle French", "Old French", "Latin", "Old Latin", "Proto-Italic"])
+# All ancestors of both ENglish and French langs, according to https://en.wiktionary.org/wiki/Category:English_language and https://en.wiktionary.org/wiki/Category:French_language
